1f460ac2082356e884831fea1eba921daa3d07b6
alunturner <alun.turner@googlemail.com>
2024-10-28 14:16:45 +0000

    building but with borked plugins

--- flake.nix
+++ flake.nix
@@ -7,13 +7,49 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    conform-nvim = {
+      url = "github:stevearc/conform.nvim?ref=1a99fdc1d3aa9ccdf3021e67982a679a8c5c740c";
+      flake = false;
+    };
+    fzf-lua = {
+      url = "github:ibhagwan/fzf-lua?ref=f513524561060f2b9e3bd6d36ff046bfa03ca114";
+      flake = false;
+    };
+    nvim-lspconfig = {
+      url = "github:neovim/nvim-lspconfig?ref=eb36e0185ad4b92b0999fb698428f2966334d2c1";
+      flake = false;
+    };
+    nvim-surround = {
+      url = "github:kylechui/nvim-surround?ref=ec2dc7671067e0086cdf29c2f5df2dd909d5f71f";
+      flake = false;
+    };
+    nvim-treesitter = {
+      url = "github:nvim-treesitter/nvim-treesitter?ref=4d94c24d6cb9985347bdf0692c1fd81088c6c8b2";
+      flake = false;
+    };
+    pax = {
+      url = "github:alunturner/pax?ref=26fcb99d9bd0e27b2b0b9fe6586181258463481f";
+      flake = false;
+    };
+    vim-tmux-navigator = {
+      url = "github:christoomey/vim-tmux-navigator?ref=a9b52e7d36114d40350099f254b5f299a35df978";
+      flake = false;
+    };
   };
 
   outputs = {
     nixpkgs,
     home-manager,
+    conform-nvim,
+    fzf-lua,
+    nvim-lspconfig,
+    nvim-surround,
+    nvim-treesitter,
+    pax,
+    vim-tmux-navigator,
     ...
-  } @ inputs: {
+  } @ inputs :
+  {
     homeConfigurations = {
       "art@maria" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.aarch64-darwin;
