c320c1d7d37992ce35cdab53d606c2b780fcfa1d
alunturner <alun.turner@googlemail.com>
2025-02-07 23:43:23 +0000

    sort out a no ligature font, update README

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -15,7 +15,7 @@ config.color_schemes = {
 config.color_scheme = "dark"
 
 -- FONT
-config.font = w.font("JetBrains Mono")
+config.font = w.font("JetBrainsMonoNl Nerd Font Mono")
 config.font_size = 15
 
 -- COMMAND PALETTE
