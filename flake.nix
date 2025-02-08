435412aa88142bb0383c681121bc48e5878037a2
alun <56027671+artcodespace@users.noreply.github.com>
2025-02-08 13:49:46 +0000

    Multiple system support (#94)
    * first go at reusability
    
    * uncomment the mac architecture
    
    * get mac and specific dependencies separated out

--- flake.nix
+++ flake.nix
@@ -5,39 +5,61 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = inputs:
   let
-    pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+    # Define some variables to make handling system specifics easier.
+    linux = { displayName= "linux"; architecture= "x86_64-linux"; };
+    mac = { displayName= "mac"; architecture= "aarch64-darwin"; };
+
+    # Helper function. Call it with one of the above objects.
+    # It will call the system specific`buildEnv` function with a name
+    # reflecting the profile being built and system specific packages.
+    buildPackage = { displayName, architecture } @ system: let
+      pkgs = inputs.nixpkgs.legacyPackages.${architecture};
+    in {
+      default = pkgs.buildEnv {
+          name = "dotfile-nix-profile-" + displayName;
+          paths = with pkgs; [
+            # COMMON
+            stow
+            tmux
+            wezterm
+            starship
+            neovim
+            fzf
+            lazygit
+            ripgrep
+            fd
+            yazi
+            jq
+            nodejs_22
+            nodePackages.nodemon
+            vscode-langservers-extracted
+            nodePackages.typescript-language-server
+            nodePackages.eslint
+            prettierd
+            lua-language-server
+            stylua
+            nixd
+            alejandra
+          ] ++ (if system == mac then
+            # MAC SPECIFIC
+            [aerospace]
+          else
+            []
+          ) ++ (if system == linux then 
+            # LINUX SPECIFIC
+            []
+          else
+            []  
+          );
+      };
+    };
   in
   {
-    packages."aarch64-darwin".default = pkgs.buildEnv {
-      name = "dotfile-nix-profile";
-      paths = with pkgs; [
-        git
-        vim
-        stow
-        tmux
-        wezterm
-        starship
-        neovim
-        fzf
-        lazygit
-        ripgrep
-        fd
-        yazi
-        jq
-        nodejs_22
-        nodePackages.nodemon
-        vscode-langservers-extracted
-        nodePackages.typescript-language-server
-        nodePackages.eslint
-        prettierd
-        lua-language-server
-        stylua
-        nixd
-        alejandra
-        aerospace
-      ];
+    packages = {
+      ${linux.architecture} = buildPackage linux;
+      ${mac.architecture} = buildPackage mac;
     };
   };
 }
