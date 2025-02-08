a5a4bd4f0c5daf1031332c7a34cbb67300ecdefc
alunturner <alun.turner@googlemail.com>
2025-02-08 13:26:40 +0000

    first go at reusability

--- flake.nix
+++ flake.nix
@@ -5,39 +5,46 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = inputs:
   let
-    pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+    linux = { displayName= "linux"; architecture= "x86_64-linux"; };
+    # mac = { displayName= "mac"; architecture= "aarch64-darwin"; };
+
+    buildPackage = { displayName, architecture }: let
+      pkgs = inputs.nixpkgs.legacyPackages.${architecture};
+    in {
+      default = pkgs.buildEnv {
+          name = "dotfile-nix-profile-" + displayName;
+          paths = with pkgs; [
+            stow
+            tmux
+            wezterm
+            starship
+            neovim
+            fzf
+            lazygit
+            ripgrep
+            fd
+            yazi
+            jq
+            nodejs_22
+            nodePackages.nodemon
+            vscode-langservers-extracted
+            nodePackages.typescript-language-server
+            nodePackages.eslint
+            prettierd
+            lua-language-server
+            stylua
+            nixd
+            alejandra
+            aerospace
+          ];
+      };
+    };
   in
   {
-    packages."aarch64-darwin".default = pkgs.buildEnv {
-      name = "dotfile-nix-profile";
-      paths = with pkgs; [
-        git
-        vim
-        stow
-        tmux
-        wezterm
-        starship
-        neovim
-        fzf
-        lazygit
-        ripgrep
-        fd
-        yazi
-        jq
-        nodejs_22
-        nodePackages.nodemon
-        vscode-langservers-extracted
-        nodePackages.typescript-language-server
-        nodePackages.eslint
-        prettierd
-        lua-language-server
-        stylua
-        nixd
-        alejandra
-        aerospace
-      ];
+    packages = {
+      ${linux.architecture} = buildPackage linux;
     };
   };
 }
