779c24aadfe2c33a0930728f0a6bcb0f761bd779
alun <56027671+artcodespace@users.noreply.github.com>
2025-04-06 09:12:43 +0100

    chore: expose unstable nixpkgs to access neovim 0.11 (#99)
    * indentation
    
    * add in unstable branch and use for neovim
    
    * add unstable to flake lock
    
    * update the starter script for tmux
    
    * add comments

--- flake.nix
+++ flake.nix
@@ -3,10 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = inputs: let
-    # Define some variables to make handling system specifics easier.
+    # Define some variables to make handling platform specifics easier.
     linux = {
       displayName = "linux";
       architecture = "x86_64-linux";
@@ -22,8 +23,9 @@
     buildPackage = {
       displayName,
       architecture,
-    } @ system: let
+    } @ platform: let
       pkgs = inputs.nixpkgs.legacyPackages.${architecture};
+      unstablePkgs = inputs.nixpkgs-unstable.legacyPackages.${architecture};
     in {
       default = pkgs.buildEnv {
         name = "dotfile-nix-profile-" + displayName;
@@ -34,7 +36,8 @@
             wezterm
             tmux
             starship
-            neovim
+            # EXPERIMENTAL - escape neovim to use unstable branch for 0.11
+            (unstablePkgs.neovim)
             fzf
             lazygit
             ripgrep
@@ -54,14 +57,16 @@
             alejandra
           ]
           ++ (
-            if system == mac
+            # NOTE there is pkgs.stdenv.isDarwin
+            if platform == mac
             then
               # MAC SPECIFIC
               [aerospace]
             else []
           )
           ++ (
-            if system == linux
+            # NOTE there is pkgs.stdenv.isLinux
+            if platform == linux
             then
               # LINUX SPECIFIC
               []
