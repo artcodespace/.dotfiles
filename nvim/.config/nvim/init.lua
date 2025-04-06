5eccbc911a5dcdf28634ef61ca15bb99f6a23b2b
alun <56027671+artcodespace@users.noreply.github.com>
2025-04-06 15:01:14 +0100

    Neovim 0.11 update fzf (#101)
    * update fzf submodule
    
    * slim down fzf config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,37 +6,16 @@ vim.g.maplocalleader = " "
 vim.keymap.set({ "n", "v" }, " ", "<nop>", { silent = true })
 
 -- SECTION: PLUGIN.FZF
--- TODO: upgrade
 local fzf = require("fzf-lua")
-local function configure_finder(title, opts)
-	return vim.tbl_deep_extend("keep", opts or {}, {
-		prompt = title .. "  ",
-		winopts = {
-			title = "┤ " .. title .. " ├",
-			title_pos = "center",
-		},
-	})
-end
-
 fzf.setup({
-	defaults = {
-		file_icons = false,
-		git_icons = false,
-	},
 	keymap = {
 		builtin = {
 			["<C-d>"] = "preview-page-down",
 			["<C-u>"] = "preview-page-up",
 		},
-		fzf = {
-			["ctrl-q"] = "select-all+accept",
-		},
 	},
-	fzf_opts = {
-		["--info"] = "default",
-		["--pointer"] = "",
-		["--marker"] = "",
-	},
+	files = { cwd_prompt = false },
+	lsp = { jump_to_single_result = true },
 	fzf_colors = {
 		["fg"] = { "fg", { "Comment" } },
 		["hl"] = { "fg", { "Normal" } },
@@ -53,24 +32,11 @@ fzf.setup({
 		["marker"] = { "fg", { "Pmenu" } },
 		["header"] = { "fg", { "Normal" } },
 	},
-	files = configure_finder("Finder", { cwd_prompt = false }),
-	buffers = configure_finder("Buffers"),
-	grep = configure_finder(
-		"Ripgrep",
-		{ rg_opts = "--column --line-number --no-heading --color=never --smart-case --max-columns=4096 -e" }
-	),
-	diagnostics = configure_finder("Diagnostics", { severity_limit = "error" }),
-	lsp = {
-		jump_to_single_result = true,
-		prompt_postfix = "  ",
-		symbols = configure_finder("Symbol"),
-	},
-	helptags = configure_finder("Help"),
+	grep = { rg_opts = "--column --line-number --no-heading --color=never --smart-case --max-columns=4096 -e" },
 })
 
 vim.keymap.set("n", "<leader>f", fzf.files)
 vim.keymap.set("n", "<leader>s", fzf.grep_project)
-vim.keymap.set("n", "<leader>d", fzf.lsp_document_diagnostics)
 vim.keymap.set("n", "<leader>o", fzf.lsp_document_symbols)
 vim.keymap.set("n", "<leader>O", fzf.lsp_live_workspace_symbols)
 vim.keymap.set("n", "<leader>r", fzf.lsp_references)
