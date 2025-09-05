d8f326d60c91275b9c0a16d2af0f2054fd7343ec
alunturner <alun.turner@googlemail.com>
2025-09-05 15:34:41 +0100

    bump flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "For use with nix profile";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
