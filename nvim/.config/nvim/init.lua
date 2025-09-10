94735d189248c8337d46f2141b380685c273fed2
alunturner <alun.turner@googlemail.com>
2025-09-10 17:40:29 +0100

    remove unnecessary mapping

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -80,7 +80,6 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.winbar = "%{%v:lua.NetrwWinBar()%}"
 		vim.keymap.set("n", "h", "-", { remap = true, buffer = true })
 		vim.keymap.set("n", "l", "<cr>", { remap = true, buffer = true })
-		vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { remap = true, buffer = true })
 	end,
 })
 
