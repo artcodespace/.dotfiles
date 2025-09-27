c5cf60b412ccf3a59214f1efe5b023a85c0bd15c
alun <56027671+artcodespace@users.noreply.github.com>
2025-09-27 14:23:22 +0100

    Improve theme handling in wezterm
    * create dark and light wezterm variants
    
    * add theme switcher
    
    * formatting

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local w = require("wezterm")
 local config = w.config_builder()
 
-config.color_scheme = "pax"
+config.color_scheme = "pax-dark"
 config.font = w.font("JetBrainsMonoNL Nerd Font Mono") -- has best icon sizes
 config.font_size = 15
 config.command_palette_font_size = 18
@@ -15,5 +15,24 @@ config.window_padding = {
 }
 config.send_composed_key_when_left_alt_is_pressed = true -- make opt+3 = # on Mac
 config.front_end = "WebGpu" -- required for proper nix rendering
+
+w.on("augment-command-palette", function()
+	return {
+		{
+			brief = "Toggle colour theme",
+			icon = "fa_sun_o",
+			action = w.action.EmitEvent("toggle-colorscheme"),
+		},
+	}
+end)
+w.on("toggle-colorscheme", function(window)
+	local overrides = window:get_config_overrides() or {}
+	if overrides.color_scheme == "pax-dark" then
+		overrides.color_scheme = "pax-light"
+	else
+		overrides.color_scheme = "pax-dark"
+	end
+	window:set_config_overrides(overrides)
+end)
 
 return config
