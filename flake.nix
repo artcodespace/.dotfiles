86030bffec2adb8e5bebf7de227e61f89bc8481a
Alun Turner <alunturner@aluns-macbook-pro.taile72a1.ts.net>
2026-03-12 16:42:21 +0000

    add unstable pkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = inputs: let
