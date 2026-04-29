19b32fce4f08776a2d39795971add6e8afa05777
Alun Turner <alun.turner@river.com>
2026-04-29 17:34:38 +0100

    WIP test out finding local dotfiles.tmux file

--- scripts/art-session.sh
+++ scripts/art-session.sh
@@ -1,1 +1,14 @@
-#!/usr/bun/env bash
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Look in pwd or home for <name>.tmux files.
+for FILE in "$PWD"/*.tmux "$HOME"/*.tmux; do
+  test -f "$FILE" && LAYOUT=$FILE && break
+done
+
+if [[ ! -f "$LAYOUT" ]]; then
+  echo "unable to find any files"
+  exit 1
+fi
+
+echo "$LAYOUT"
