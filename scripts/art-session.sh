7e810b1c19fe07876cc010753d22db7eb84db88b
Alun Turner <alun.turner@river.com>
2026-04-29 17:51:55 +0100

    pull out the session name from the config file

--- scripts/art-session.sh
+++ scripts/art-session.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+ext=".tmux"
+
 # Look in pwd or home for <name>.tmux files.
-for FILE in "$PWD"/*.tmux "$HOME"/*.tmux; do
-  test -f "$FILE" && LAYOUT=$FILE && break
+for file in "$PWD"/*"$ext" "$HOME"/*"$ext"; do
+  test -f "$file" && config=$file && break
 done
 
-# Exit if we can't find anything
-[[ -f "${LAYOUT:-}" ]] || { echo "No .tmux files found"; exit 1; }
+# Exit if we can't find any valid file.
+[[ -f "${config:-}" ]] || { echo "No $ext files found"; exit 1; }
 
-echo "$LAYOUT"
+# Name is either what user passes or the <name> from the config file.
+session=${1:-$(basename "$config" $ext)}
+echo "$session"
+echo "$config"
