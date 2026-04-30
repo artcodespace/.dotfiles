8480220ec8be781fd757357858f36760713f08fd
Alun Turner <alun.turner@river.com>
2026-04-30 11:11:05 +0100

    add scratch documentation

--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# first: window(*)
+# - second: window-name
+# - third?: horizontal | vertical
+# first: pane(*)
+# - second?: command
 while read -r first second third; do
   echo "$second $first $third"
 done < "$PWD"/test.tmux
