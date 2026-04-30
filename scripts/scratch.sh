11514286d63d0569c9c621dba55e003b7871cb2f
Alun Turner <alun.turner@river.com>
2026-04-30 14:15:45 +0100

    get orientation working

--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 active_window=""
 active_pane=""
 line=0
+current_window=""
+current_orientation="-v"
 current_pane=1 # depends on the numbering in my config
 session="test" # will be available in scope in real call
 
@@ -20,16 +22,32 @@ while read -r first second third; do
   if [[ "$first" == "window"* ]]; then
     # Validate the required input
     [[ -n "$second" ]] || { echo "Usage: window <window-name>, line $line"; exit 1; }
-    
-    # Update active window if required
+    [[ -z "$third" || "$third" == "horizontal" || "$third" == "vertical" ]] || { echo "Error: layout must be either horizontal or vertical, line $line"; exit 1; }
+
+    # Maybe update active window
     [[ "$first" == *"*" ]] && active_window=$second
+
+    # Maybe update current_orientation
+    if [[ "$third" == "horizontal" ]]; then
+      current_orientation="-v"
+    else
+      current_orientation="-h"
+    fi
 
     # Add a window
     echo "window command >>> tmux new-window -a -t $session -n $second -c $PWD"
+
+    # Set current window and reset pane count
+    current_window="$second"
+    current_pane=1
   elif [[ "$first" == "pane"* ]]; then
     # Update active pane if required
-    [[ "$first" == *"*" ]] && active_window=$second
-    echo "pane >>> $first $second $third"
+    [[ "$first" == *"*" ]] && active_pane=$current_pane
+
+    # Add a pane
+    echo "pane command >>> tmux split-window -t \"$session\":\"$current_window\" $current_orientation"
+
+    current_pane=$((current_pane + 1))
   else
     echo "can not parse"
   fi
